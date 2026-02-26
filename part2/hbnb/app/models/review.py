a92cde7cd8e473ca20afad0dc71d47fb8dd11da1
Gwendal-B <12309@holbertonstudents.com>
2026-02-26 18:10:33 +0100

    fix: fixes the issue of not being able to delete a review and also adds the functionality to update a review

--- part2/hbnb/app/models/review.py
+++ part2/hbnb/app/models/review.py
@@ -5,6 +5,7 @@ from app.models.base_model import BaseModel
 class Review(BaseModel):
     def __init__(self, text, rating, place, user):
         super().__init__()
+        self.validate_rating(rating)
         self.text = text
         self.rating = rating  # Devrait être validé (1-5)
         self.place = place   # Instance de Place
